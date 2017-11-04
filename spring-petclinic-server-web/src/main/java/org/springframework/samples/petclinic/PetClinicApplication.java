a841f55051d636c87fac221edf36cde4ebbd7e44
venkatesh29 <venkatesh.sajjanapu@yottatechports.com>
2017-11-03 21:44:59 -0500

    config changes to deployable in tomcat as war

--- spring-petclinic-server-web/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ spring-petclinic-server-web/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -1,8 +1,11 @@
 package org.springframework.samples.petclinic;
 
+import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.samples.petclinic.config.PetclinicProperties;
 import org.springframework.ui.ModelMap;
@@ -13,9 +16,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 @EnableConfigurationProperties(PetclinicProperties.class)
-public class PetClinicApplication {
+public class PetClinicApplication extends SpringBootServletInitializer{
 
-    public static void main(String[] args) {
+	
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
+    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+		return builder.sources(PetClinicApplication.class).bannerMode(Mode.OFF);
+    }
+	public static void main(String[] args) {
         SpringApplication.run(PetClinicApplication.class, args);
     }
 }
